f31450408369f5d2e088216a41099ac1a6142131
MarsTrip <jagetau@aliyun.com>
2020-08-28 10:09:28 +0800

    测试一下a

---
+++ src/AssignOS/test.java
@@ -0,0 +1,20 @@
+package AssignOS;
+
+import sun.tracing.PrintStreamProviderFactory;
+
+import java.io.FileReader;
+import java.nio.file.FileStore;
+
+public class test {
+    public static final
+    private static final
+    public static final int ab = 3;
+    pric
+}
+    public static void main(String[] args){
+        System.out.println("dsf");
+
+}
+        }
+    }
+}
